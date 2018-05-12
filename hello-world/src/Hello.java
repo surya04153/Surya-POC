f7f6409c7da0f2e78253760b2001022434c148dc
surya panda <suryanarayan.panda@gmail.com>
2018-05-12 06:40:52 +0000

    commiting changes.

--- hello-world/src/Hello.java
+++ hello-world/src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hiiii Surya say bye bye to bit coin");
+		System.out.println("hiiii Surya say bye bye to lite coin");
 	}
 
 }
